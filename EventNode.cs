708be18e6a680f2f7a19951e7b72aa0e997f525e
wolfcrafter321 <wolfcrafter321@gmail.com>
2022-07-03 00:03:29 +0900

    edit

--- EventNode.cs
+++ EventNode.cs
@@ -7,7 +7,7 @@ public class EventNode : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hi");
+        Debug.Log("Hiiii");
     }
 
     // Update is called once per frame
